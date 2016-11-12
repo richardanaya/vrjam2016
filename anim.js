4737a89ace1b5bf26dcb135d81db60c303c4e133
trishkatz <katz.tricia@gmail.com>
2016-11-12 15:32:42 -0600

    Candidate Facts

--- anim.js
+++ anim.js
@@ -2,10 +2,19 @@ document.body.querySelector("#turtle").addEventListener("click",function(){
   alert("turtle");
 })
 
+	var counter = document.querySelector("#turtle-counter");
+	var myIndex = 1;
+	var turtleFacts = ["I have a hard shell.\nHard shells are STRONG!", "I'm slow to act.\nI don't jump the gun on big decisions!", "Green is a power color."];
+
+document.body.querySelector("#turtle-counter").addEventListener("click", function(){
+	var fact = turtleFacts[myIndex++%turtleFacts.length];
+	counter.setAttribute("bmfont-text", "text:" + fact);
+})
+
 function updateVotes(votes){
   document.body.querySelector("#turtle-votes").setAttribute('scale', {x: 1, y: votes.turtle/10, z: 1});
   document.body.querySelector("#tiger-votes").setAttribute('scale', {x: 1, y: votes.tiger/10, z: 1});
   document.body.querySelector("#rabbit-votes").setAttribute('scale', {x: 1, y: votes.rabbit/10, z: 1});
 }
-updateVotes(voting.getVotes())
+updateVotes(voting.getVotes());
 voting.onVotesUpdate(updateVotes);
