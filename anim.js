d55e6699a7f9e61d5cf1b6cc6b47722fde32f0f8
Richard <richard.anaya@gmail.com>
2016-11-12 13:24:31 -0600

    making clicable

--- anim.js
+++ anim.js
@@ -1,5 +1,8 @@
+document.body.querySelector("#turtle").addEventListener("click",function(){
+  alert("turtle");
+})
+
 function updateVotes(votes){
-  debugger;
   document.body.querySelector("#turtle-votes").setAttribute('scale', {x: 1, y: votes.turtle/10, z: 1});
   document.body.querySelector("#tiger-votes").setAttribute('scale', {x: 1, y: votes.tiger/10, z: 1});
   document.body.querySelector("#rabbit-votes").setAttribute('scale', {x: 1, y: votes.rabbit/10, z: 1});
