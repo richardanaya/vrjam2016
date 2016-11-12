4ae9ef223f9d4ae89485a6af0c01e91e27426bf1
Richard <richard.anaya@gmail.com>
2016-11-12 13:09:40 -0600

    voting

--- anim.js
+++ anim.js
@@ -1,2 +1,8 @@
-var cat = document.body.querySelector("#rabbit-votes");
-cat.setAttribute('scale', {x: 1, y: .5, z: 1});
+function updateVotes(votes){
+  debugger;
+  document.body.querySelector("#turtle-votes").setAttribute('scale', {x: 1, y: votes.turtle/10, z: 1});
+  document.body.querySelector("#tiger-votes").setAttribute('scale', {x: 1, y: votes.tiger/10, z: 1});
+  document.body.querySelector("#rabbit-votes").setAttribute('scale', {x: 1, y: votes.rabbit/10, z: 1});
+}
+updateVotes(voting.getVotes())
+voting.onVotesUpdate(updateVotes);
